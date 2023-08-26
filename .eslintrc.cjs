6b42f6a55c6a996bd6e729bf17ce95c598740700
Manjot Virdi <virdi_manjot@hotmail.com>
2023-08-25 18:46:07 -0700

    add in pre commit hook and install prettier and eslint; make some fixes from eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,27 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended-type-checked",
-        "plugin:react-hooks/recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+    extraFileExtensions: ['.json'],
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    ignorePatterns: ["dist", ".eslintrc.cjs"],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        project: ["./tsconfig.json", "./tsconfig.node.json"],
-        tsconfigRootDir: __dirname,
-    },
-    plugins: ["react-refresh"],
-    rules: {
-        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    },
+  },
 };
--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,38 +1,42 @@
-import { Task } from "../../utils/types";
-import deleteIcon from "../../assets/icon-delete.svg";
+import { Task } from '../../utils/types';
+import deleteIcon from '../../assets/icon-delete.svg';
 
 interface TaskProps {
-    task: Task;
-    handleTaskCompletionChange: (taskId: string) => void;
-    handleSingleTaskDelete: (taskId: string) => void;
+  task: Task;
+  handleTaskCompletionChange: (taskId: string) => Promise<void>;
+  handleSingleTaskDelete: (taskId: string) => Promise<void>;
 }
 
-function TaskItem({ task, handleTaskCompletionChange, handleSingleTaskDelete }: TaskProps) {
-    const taskId = `task-${task._id}`; // Unique ID for the input element
+function TaskItem({
+  task,
+  handleTaskCompletionChange,
+  handleSingleTaskDelete,
+}: TaskProps) {
+  const taskId = `task-${task._id}`; // Unique ID for the input element
 
-    return (
-        <div className="flex hover:bg-gray-300 px-2 rounded-lg group">
-            <div className="w-full flex gap-4">
-                <input
-                    className="checked:accent-white cursor-pointer"
-                    type="checkbox"
-                    name="task"
-                    id={taskId}
-                    checked={task.completed}
-                    onChange={() => handleTaskCompletionChange(task._id)}
-                />
-                <label className="cursor-pointer w-full" htmlFor={taskId}>
-                    {task.task}
-                </label>
-            </div>
-            <img
-                className="lg:hidden group-hover:block w-3 hover:w-4 cursor-pointer"
-                src={deleteIcon}
-                alt="delete"
-                onClick={() => handleSingleTaskDelete(task._id)}
-            />
-        </div>
-    );
+  return (
+    <div className="flex hover:bg-gray-300 px-2 rounded-lg group">
+      <div className="w-full flex gap-4">
+        <input
+          className="checked:accent-white cursor-pointer"
+          type="checkbox"
+          name="task"
+          id={taskId}
+          checked={task.completed}
+          onChange={() => void handleTaskCompletionChange(task._id)}
+        />
+        <label className="cursor-pointer w-full" htmlFor={taskId}>
+          {task.task}
+        </label>
+      </div>
+      <img
+        className="lg:hidden group-hover:block w-3 hover:w-4 cursor-pointer"
+        src={deleteIcon}
+        alt="delete"
+        onClick={() => void handleSingleTaskDelete(task._id)}
+      />
+    </div>
+  );
 }
 
 export default TaskItem;
