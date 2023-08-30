82e7cd25bf1fcd7a606522192db07b11f180ec10
Manjot Virdi <virdi_manjot@hotmail.com>
2023-08-29 22:34:17 -0600

    add modal messages

--- src/components/DeleteModalUI.tsx
+++ src/components/DeleteModalUI.tsx
@@ -1,3 +1,10 @@
+const MODAL_MESSAGES = {
+  confirmDeleteAll: 'Are you sure you want to delete all tasks?',
+  noTasksToDelete: 'No tasks to delete.',
+  closeButton: 'Close',
+  deleteButton: 'Delete',
+};
+
 interface DeleteModalUIProps {
   handleDeleteAll: () => Promise<void>;
   handleCloseModal: () => void;
@@ -13,22 +20,22 @@ function DeleteModalUI({
     <div className="h-full w-full flex flex-col justify-center items-center gap-10">
       <p>
         {tasksExist
-          ? 'Are you sure you want to delete all tasks?'
-          : 'No tasks to delete.'}
+          ? MODAL_MESSAGES.confirmDeleteAll
+          : MODAL_MESSAGES.noTasksToDelete}
       </p>
       <div className="w-full flex flex-col md:flex-row md:justify-center gap-5">
         <div
           className="cursor-pointer border border-black text-center p-2 rounded-lg w-full md:max-w-xs hover:bg-cyan-100"
           onClick={handleCloseModal}
         >
-          Close
+          {MODAL_MESSAGES.closeButton}
         </div>
         {tasksExist && (
           <div
             className="cursor-pointer border border-black text-center p-2 rounded-lg w-full md:max-w-xs hover:bg-cyan-100"
             onClick={() => void handleDeleteAll()}
           >
-            Delete
+            {MODAL_MESSAGES.deleteButton}
           </div>
         )}
       </div>
